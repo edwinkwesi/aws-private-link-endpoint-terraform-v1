c114e43684d91db7be7ce8bfb3d50c34eb3851fc
edwinkwesi <amooedwin2@gamil.com>
2024-05-23 06:57:23 +0100

    added topology design modification

--- consumer-private-sg.tf
+++ consumer-private-sg.tf
@@ -1,14 +1,14 @@
-resource "aws_security_group" "consumer-sg" {
-    name   = "SSH Access from bastion"
-    vpc_id      = aws_vpc.consumer-vpc.id
+# resource "aws_security_group" "consumer-sg" {
+#     name   = "SSH Access from bastion"
+#     vpc_id      = aws_vpc.consumer-vpc.id
 
   
 
-  ingress {
-    # description     = "SSH From Anywhere or Your-IP"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.consumer-pub-ec2-sg.id]
-  }
-}
+#   ingress {
+#     # description     = "SSH From Anywhere or Your-IP"
+#     from_port       = 22
+#     to_port         = 22
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.consumer-pub-ec2-sg.id]
+#   }
+# }
